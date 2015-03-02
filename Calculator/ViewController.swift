ddebaa8aaa23a6ad08c8af9f4b34fc429c4aaabe
Nakib Momin <nbmomin14@gmail.com>
2015-03-02 01:58:36 -0500

    Added return key and its functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,24 +15,42 @@ class ViewController: UIViewController {
     //keeping track if user is in middle of typing
     var userIsTyping = false;
     
+    //append number to display screen
     @IBAction func appendNumber(sender: UIButton) {
         //get user pressed number
         let digit = sender.currentTitle!
         
         //if user is in middle of typing append digit or set display value to one entered
-        if(userIsTyping)
-        {
+        if(userIsTyping){
             display.text = display.text! + digit
         }
-        else
-        {
+        else{
             display.text = digit
             userIsTyping = true
         }
        
         print(digit);
     }
-
-
+    
+    //stack to hold user inputed values
+    var operandStack = Array<Double>()
+    
+    //Adding value to stack on pressing return
+    @IBAction func enter() {
+        userIsTyping = false
+        operandStack.append(displayValue)
+        print(operandStack);
+    }
+    
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set{
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
 }
 
